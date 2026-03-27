ddbee852fd2babf7d11964b2f42b8973bec19407
maikel <madomingueza@copaair.com>
2026-03-27 10:42:54 -0500

    Pruebas de multiplicación y división

--- calculadora.py
+++ calculadora.py
@@ -12,13 +12,8 @@ class Calculadora:
     def multiplicacion(self, a, b):
         return a * b
     
-def suma(a, b):
-    return a + b
-
-def resta(a, b):
-    return a - b
-
-def multiplicacion(a, b):
-    return a * b
-
-
+    def division(self, a, b):
+        if b == 0:
+            raise ValueError("No se puede dividir por cero")
+        return a / b
+    
