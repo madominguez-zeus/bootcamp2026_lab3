cfba4dca89a8207f5f38b518813682007845f0be
maikel <madomingueza@copaair.com>
2026-03-27 10:03:27 -0500

    Segunda prueba + .gitignore

--- test_calculadora.py
+++ test_calculadora.py
@@ -1,7 +1,12 @@
 import pytest
 from unittest.mock import Mock
-from calculadora import Calculadora, suma
+from calculadora import Calculadora, suma, resta, multiplicacion
 
 def test_suma():
     calc = Calculadora()
     assert calc.suma(2, 3) == 5
+
+def test_resta():
+    calc = Calculadora()
+    assert calc.resta(5, 2) == 3
+
